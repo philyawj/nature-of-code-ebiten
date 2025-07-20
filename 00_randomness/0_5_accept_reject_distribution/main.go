fbc1d1385f2e1cc17193743ba356ab1c200a9bcd
Jordan Philyaw <jordanphilyaw@gmail.com>
2025-07-20 10:57:02 -0400

    Example 0.5

---
+++ 00_randomness/0_5_accept_reject_distribution/main.go
@@ -0,0 +1,74 @@
+// Copyright 2025 Jordan Philyaw
+//
+// Port of Example 0.5 from "The Nature of Code" by Daniel Shiffman,
+// published by No Starch Press® Inc., licensed under CC BY-NC-SA 4.0.
+// Rewritten in Go using the Ebitengine game engine.
+
+package main
+
+import (
+	"image/color"
+	"math/rand"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/vector"
+)
+
+const (
+	screenWidth  = 640
+	screenHeight = 240
+	numBars      = 20
+)
+
+type Game struct {
+	barCounts [numBars]int
+}
+
+func NewGame() *Game {
+	return &Game{}
+}
+
+func acceptReject() float64 {
+	for {
+		r1 := rand.Float64()
+		probability := r1
+		r2 := rand.Float64()
+		if r2 < probability {
+			return r1
+		}
+	}
+}
+
+func (g *Game) Update() error {
+	index := int(acceptReject() * float64(numBars))
+	g.barCounts[index]++
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	screen.Fill(color.White)
+
+	barWidth := float32(screenWidth) / float32(numBars)
+	for x := range g.barCounts {
+		barHeight := float32(g.barCounts[x])
+		rectX := float32(x) * barWidth
+		rectY := float32(screenHeight) - barHeight
+		rectWidth := barWidth
+		rectHeight := barHeight
+
+		vector.DrawFilledRect(screen, rectX, rectY, rectWidth, rectHeight, color.RGBA{127, 127, 127, 255}, false)
+		vector.StrokeRect(screen, rectX, rectY, rectWidth, rectHeight, 2, color.Black, false)
+	}
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return screenWidth, screenHeight
+}
+
+func main() {
+	ebiten.SetWindowSize(screenWidth, screenHeight)
+	ebiten.SetWindowTitle("An Accept-Reject Distribution")
+	if err := ebiten.RunGame(NewGame()); err != nil {
+		panic(err)
+	}
+}
