0f4fca8d1f08f956cd360896323e17599a9dcda3
Jordan Philyaw <jordanphilyaw@gmail.com>
2025-08-04 21:10:44 -0400

    1.5 refactor draw into update

--- 01_vectors/1_5_vector_magnitude/main.go
+++ 01_vectors/1_5_vector_magnitude/main.go
@@ -20,33 +20,37 @@ const (
 	screenHeight = 240
 )
 
-type game struct{}
+type game struct {
+	mouseVec, centerVec, subVec *p5math.Vector
+	mag                         float32
+}
 
 func newGame() *game {
-	return &game{}
+	return &game{
+		mouseVec:  p5math.NewVector(0, 0),
+		centerVec: p5math.NewVector(float32(screenWidth)/2, float32(screenHeight)/2),
+		subVec:    p5math.NewVector(0, 0),
+		mag:       0,
+	}
 }
 
 func (g *game) Update() error {
+	mouseX, mouseY := ebiten.CursorPosition()
+	boundedX := util.ConstrainToScreen(mouseX, screenWidth)
+	boundedY := util.ConstrainToScreen(mouseY, screenHeight)
+	g.mouseVec = p5math.NewVector(float32(boundedX), float32(boundedY))
+
+	g.subVec = p5math.SubVectors(g.mouseVec, g.centerVec)
+	g.mag = g.subVec.Mag()
+
 	return nil
 }
 
 func (g *game) Draw(screen *ebiten.Image) {
 	screen.Fill(color.White)
 
-	mouseX, mouseY := ebiten.CursorPosition()
-	boundedX := util.ConstrainToScreen(mouseX, screenWidth)
-	boundedY := util.ConstrainToScreen(mouseY, screenHeight)
-	mouse := p5math.NewVector(float32(boundedX), float32(boundedY))
-
-	center := p5math.NewVector(float32(screenWidth)/2, float32(screenHeight)/2)
-
-	mouse.Sub(center)
-
-	vector.StrokeLine(screen, center.X, center.Y, center.X+mouse.X, center.Y+mouse.Y, 2, color.Black, true)
-
-	mag := mouse.Mag()
-	vector.DrawFilledRect(screen, 10, 10, mag, 10, color.Black, false)
-
+	vector.StrokeLine(screen, g.centerVec.X, g.centerVec.Y, g.centerVec.X+g.subVec.X, g.centerVec.Y+g.subVec.Y, 2, color.Black, true)
+	vector.DrawFilledRect(screen, 10, 10, g.mag, 10, color.Black, false)
 }
 
 func (g *game) Layout(outsideWidth, outsideHeight int) (int, int) {
